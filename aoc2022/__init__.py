ce760bdd16f0a2ad4a0fdd6a967b40219071e006
polkel <104925382+polkel@users.noreply.github.com>
2023-11-02 20:48:45 -0400

    added menu options to aoc2022 and initialized day03 folder

--- aoc2022/__init__.py
+++ aoc2022/__init__.py
@@ -1,14 +1,61 @@
 import importlib
 import os
+from .helpers import create_new_day
 
 
 def main():
     os.system("")
 
-    user_input = input("Which day would you like to see?\n").strip()
+    welcome_message = """\n\nWelcome to my advent of code 2022 library. Please choose a menu item:
+    
+    1. View problem and solution for a given day
+    2. Create a directory for a new day
+    3. Exit\n\n"""
 
-    if int(user_input) < 10:
-        user_input = "0" + user_input
-    print()
-    day_module = importlib.import_module(".day" + user_input, "aoc2022")
-    day_module.main()
+    menu_options = [view_day, create_day, exit_aoc]
+
+    try:
+        user_input = int(input(welcome_message))
+        print("\n\n")
+        menu_item = menu_options[user_input - 1]
+
+    except (ValueError, IndexError):
+        print("Please enter a valid menu item \n\n")
+        main()
+
+    else:
+        menu_item()
+
+
+def view_day():
+    user_input = input("Which day would you like to see?\n\n").strip()
+    print("\n\n")
+
+    try:
+        if int(user_input) < 10:
+            user_input = "0" + user_input
+        day_module = importlib.import_module(".day" + user_input, "aoc2022")
+    except ModuleNotFoundError:
+        print("That day does not exist. Going back to the main menu.\n")
+    else:
+        day_module.main()
+    finally:
+        main()
+
+
+def create_day():
+    try:
+        user_input = int(input("Which day would you like to create?\n\n"))
+        print("\n\n")
+        if user_input < 1 or user_input > 31:
+            raise ValueError
+    except ValueError:
+        print("Please enter a valid day\n")
+    else:
+        create_new_day(user_input)
+    finally:
+        main()
+
+
+def exit_aoc():
+    print("Goodbye!")
--- aoc2022/helpers.py
+++ aoc2022/helpers.py
@@ -1,4 +1,5 @@
 import textwrap
+import os
 
 
 def print_to_display(text, bold=False):
@@ -14,3 +15,22 @@ def read_problem(module_file_path):
         for line in file:
             print_to_display(line)
     print("\n")
+
+
+def create_new_day(day_num: int):
+    curr_dir = os.path.dirname(__file__)
+    if day_num < 10:
+        dir_name = f"day0{str(day_num)}"
+    else:
+        dir_name = f"day{str(day_num)}"
+    dir_items = os.listdir(curr_dir)
+    if dir_name in dir_items:
+        print("That day already exists!\n\n")
+    else:
+        os.chdir(curr_dir)
+        os.mkdir(dir_name)
+        os.chdir(dir_name)
+        with open("__init__.py", "w") as file:
+            file.write("def main():\n")
+            file.write("    pass\n")
+        print("New directory created successfully!\n\n")
